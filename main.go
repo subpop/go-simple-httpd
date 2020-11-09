f37a89fa9702afd9d0b52c5df187b585c0b851ea
Link Dupont <link@sub-pop.net>
2020-11-09 11:35:16 -0500

    add verbose option to print request body

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 )
+
+var config struct {
+	addr    string
+	dir     string
+	verbose bool
+}
 
 func main() {
 	homeDir, err := os.UserHomeDir()
@@ -14,13 +21,27 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var addr string
-	flag.StringVar(&addr, "addr", ":8888", "listen on the provided address and port")
-
-	var dir string
-	flag.StringVar(&dir, "dir", filepath.Join(homeDir, "public_html"), "directory of files")
+	flag.StringVar(&config.addr, "addr", ":8888", "listen on the provided address and port")
+	flag.StringVar(&config.dir, "dir", filepath.Join(homeDir, "public_html"), "directory of files")
+	flag.BoolVar(&config.verbose, "verbose", false, "print request bodies")
 
 	flag.Parse()
 
-	log.Fatal(http.ListenAndServe(addr, http.FileServer(http.Dir(dir))))
+	log.Fatal(http.ListenAndServe(config.addr, logRequest(http.FileServer(http.Dir(config.dir)))))
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%v %v %v", r.RemoteAddr, r.Method, r.URL)
+		if config.verbose {
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				log.Println(err)
+			}
+			defer r.Body.Close()
+
+			log.Println(string(body))
+		}
+		handler.ServeHTTP(w, r)
+	})
 }
