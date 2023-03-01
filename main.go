562eab677148a19432e2d6787754fdbade51d7a4
Link Dupont <link@sub-pop.net>
2023-03-01 14:35:42 -0500

    feat: add lag option to lag each HTTP response

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 var config struct {
 	addr    string
 	dir     string
 	verbose bool
+	lag     time.Duration
 }
 
 func main() {
@@ -24,10 +26,19 @@ func main() {
 	flag.StringVar(&config.addr, "addr", ":8888", "listen on the provided address and port")
 	flag.StringVar(&config.dir, "dir", filepath.Join(homeDir, "public_html"), "directory of files")
 	flag.BoolVar(&config.verbose, "verbose", false, "print request bodies")
+	flag.DurationVar(&config.lag, "lag", 0, "sleep for `duration` before each HTTP response")
 
 	flag.Parse()
 
-	log.Fatal(http.ListenAndServe(config.addr, logRequest(http.FileServer(http.Dir(config.dir)))))
+	log.Fatal(http.ListenAndServe(config.addr, lagRequest(logRequest(http.FileServer(http.Dir(config.dir))))))
+}
+
+func lagRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("sleeping for %v", config.lag)
+		time.Sleep(config.lag)
+		handler.ServeHTTP(w, r)
+	})
 }
 
 func logRequest(handler http.Handler) http.Handler {
